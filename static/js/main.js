ed93ad81ea1ae6e1a410ba3810f1ec87dea27ab1
Derek.S <derekseli@outlook.com>
2018-08-09 20:38:24 +0800

    updatre stylecss

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(
+    function () {
+        /* init Search Select */
+        $('#Search_S').selectpicker({
+            style: 'btn-info',
+            size: 4
+        });
+    }
+)
