ba1b3ce40428dec8f0715b49183c15e1feab4ac3
Derek.S <derekseli@outlook.com>
2018-08-10 15:52:35 +0800

    修改数据库，增加测试代码

--- model.py
+++ model.py
@@ -16,9 +16,21 @@ def init_idRecode():
         'id': 0
     }
     dbCollectTest = mongo.db.collection_names()
-    if "idRecode" not in dbCollectTest:
-        mongo.db.IPAMS_idRecode.insert_one(initID)
+    print(dbCollectTest)
+    if "IPRMS_idRecode" not in dbCollectTest:
+        mongo.db.IPRMS_idRecode.insert_one(initID)
         status = 0
     else:
         status = 1
     return status
+
+
+
+def getID():
+    """
+    获取自定义ID的值
+    :return:
+    """
+    id = mongo.db.IPRMS_idRecode.find_one()["id"]
+    return id
+
