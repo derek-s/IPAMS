474d9588a0d26f8044f016ed614bc5ed5ab956b9
Derek.S <derekseli@outlook.com>
2018-08-18 01:52:07 +0800

    增加导入CSV及上传代码

--- views/index.py
+++ views/index.py
@@ -6,8 +6,14 @@
 # @File    : index.py
 
 from flask import render_template, request, Blueprint, abort
-from model import IPResViews, paginate, searchAll, serachPrecise
+from model import IPResViews, paginate, searchAll
 from search import search
+from upload import checkFileName, ListfileName, importCSVToDB, createUID
+from importCSV import getTaskStatus
+from app import app, executor
+import os
+import json
+
 
 indexViews = Blueprint("indexViews", __name__)
 
@@ -77,3 +83,53 @@ def Search():
         return search(KeyWord, pageNum, Model, "City")
     else:
         abort(404)
+
+@indexViews.route("/upload", methods=["GET", "POST"])
+def uploadCSV():
+    """
+    上传CSV
+    :return:
+    """
+    if(request.method == "GET"):
+        return render_template("upload.html")
+    elif(request.method == "POST"):
+        file = request.files['file']
+        print(file)
+        if(file and checkFileName(file.filename)):
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
+            return "上传完成"
+        else:
+            return "未选择文件或文件名不符合规定"
+
+
+@indexViews.route("/upload/getFileList", methods=["GET","POST"])
+def getFileList():
+    """
+    返回uploads文件夹内的已上传文件
+    :return:
+    """
+    FileList = ListfileName(app.config["UPLOAD_FOLDER"])
+    return render_template("Filelist.html", FileList=FileList)
+
+@indexViews.route("/upload/start", methods=["POST"])
+def importStart():
+    """
+    启动导入
+    :return:
+    """
+    uuid = createUID()
+    jsonDatas = request.get_json()
+    fileName = jsonDatas["fileName"]
+    executor.submit(importCSVToDB, fileName, uuid)
+    status = {
+        "status": 1,
+        "taskid": uuid
+    }
+    return json.dumps(status)
+
+@indexViews.route("/upload/status", methods=["POST"])
+def getStatus():
+    jsonDatas = request.get_json()
+    taskid = jsonDatas["taskid"]
+    result = getTaskStatus(taskid)
+    return result
